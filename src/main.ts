3b566ff60ba539af3e236540a30579cc912cf58b
woorld <17719812+woorld@users.noreply.github.com>
2025-08-12 22:08:36 +0900

    Vuetifyのセットアップ

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,20 @@
 import { createApp } from 'vue';
-import App from './App.vue';
 import './assets/style.css';
+import App from './App.vue';
+import 'vuetify/styles';
+import { createVuetify } from 'vuetify';
+import * as components from 'vuetify/components';
+import * as directives from 'vuetify/directives';
+import '@mdi/font/css/materialdesignicons.css';
 
-createApp(App).mount('#app');
+const vuetify = createVuetify({
+  components,
+  directives,
+  theme: {
+    defaultTheme: 'system',
+  },
+});
+
+createApp(App)
+  .use(vuetify)
+  .mount('#app');
